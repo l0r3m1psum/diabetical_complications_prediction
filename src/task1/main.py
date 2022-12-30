863abdfa2c47ede656e2f976036f12b516eb89b1
l0r3m1psum <bellanidiego@gmail.com>
2022-12-30 09:36:59 +0100

    Cleanup.

--- src/task1/main.py
+++ src/task1/main.py
@@ -36,81 +36,86 @@ codice_stitch = pandas.Categorical(f'STITCH{i:03}' for i in range(1, 6))
 # TODO: idcentro can probably be and int16 and idana can probabbly be an int32
 # to use less memory.
 
-anagraficapazientiattivi = anagraficapazientiattivi.drop(anagraficapazientiattivi.columns[0], axis=1) #removing row number from dataset
+# The first colum of each table contains the row number which is useless for us.
+def remove_first_column(df: pandas.DataFrame) -> pandas.DataFrame:
+	res = df.drop(df.columns[0], axis=1)
+	return res
 
-#setting category and types of data
+anagraficapazientiattivi = remove_first_column(anagraficapazientiattivi)
 anagraficapazientiattivi.annodiagnosidiabete = pandas.to_datetime(anagraficapazientiattivi.annodiagnosidiabete.astype('Int16'), format='%Y')
 anagraficapazientiattivi.annonascita = pandas.to_datetime(anagraficapazientiattivi.annonascita, format='%Y')
 anagraficapazientiattivi.annoprimoaccesso = pandas.to_datetime(anagraficapazientiattivi.annoprimoaccesso.astype('Int16'), format='%Y')
 anagraficapazientiattivi.annodecesso = pandas.to_datetime(anagraficapazientiattivi.annodecesso.astype('Int16'), format='%Y')
 anagraficapazientiattivi.sesso = anagraficapazientiattivi.sesso.astype('category')
 anagraficapazientiattivi.tipodiabete = anagraficapazientiattivi.tipodiabete.astype('category')
+# TODO: understand scolarita ,statocivile, professione, origine
 
 #we can remove the feature "tipodiabete" since every instance of the dataset has the same value (5)
-print("does tipodiabete contain always the same value? ",len(anagraficapazientiattivi) == len(anagraficapazientiattivi[anagraficapazientiattivi.tipodiabete == 5]))
+print("Does tipodiabete contain always the same value? ", (anagraficapazientiattivi.tipodiabete == 5).all())
 
-# TODO: understand scolarita ,statocivile, professione, origine
-#checking that idcentro,idana are actually a primary key
+# Checking that (idcentro, idana) is a primary key.
 if (anagraficapazientiattivi.groupby(['idcentro', 'idana']).size() != 1).any():
 	raise Exception("(idcentro, idana) are not the primary key for anagraficapazientiattivi")
 anagraficapazientiattivi = anagraficapazientiattivi.set_index(['idcentro', 'idana'])
 
-#Invalid feature cleaning
-n_invalid_patients = anagraficapazientiattivi[anagraficapazientiattivi.annonascita >= anagraficapazientiattivi.annodecesso]
+# Invalid feature cleaning
+mask = anagraficapazientiattivi.annonascita >= anagraficapazientiattivi.annodecesso
+n_invalid_patients = (mask).sum()
 anagraficapazientiattivi = anagraficapazientiattivi.drop(
-	anagraficapazientiattivi[anagraficapazientiattivi.annonascita >= anagraficapazientiattivi.annodecesso].index
+	anagraficapazientiattivi[mask].index
 )
 assert not (anagraficapazientiattivi.annonascita >= anagraficapazientiattivi.annodecesso).any()
 # TODO: check that annodiagnosidiabete and annoprimoaccesso are between birth and death.
-
+print("Remaining patients after eliminating inconsistent birth and deaths:", len(anagraficapazientiattivi), "patients eliminated:", n_invalid_patients)
 #patient remaining TODO class distribution (i.e. with or without cardiovascular events)
-print("Remaining patients after eliminating inconsistent birth and deaths: ",len(anagraficapazientiattivi), " patients eliminated: ", n_invalid_patients)
-#all the tables must contain ONLY active patients
-patients = anagraficapazientiattivi.index.to_frame().reset_index(drop=True)# Used to make sure that there are no patience outside of this group in the other tables
+del mask, n_invalid_patients
 
-diagnosi = diagnosi.drop(diagnosi.columns[0], axis=1)
+# Used to make sure that there are no patience outside of this group in the other tables.
+patients = anagraficapazientiattivi.index.to_frame().reset_index(drop=True)
+
+diagnosi = remove_first_column(diagnosi)
 diagnosi = diagnosi.merge(patients)
 diagnosi.data = pandas.to_datetime(diagnosi.data)
 diagnosi.codiceamd = diagnosi.codiceamd.astype(codice_amd.dtype)
 # TODO: understand valore
 # wtf = diagnosi.valore[~diagnosi.valore.apply(is_float)].value_counts()
 
-esamilaboratorioparametri = esamilaboratorioparametri.drop(esamilaboratorioparametri.columns[0], axis=1)
+esamilaboratorioparametri = remove_first_column(esamilaboratorioparametri)
 esamilaboratorioparametri = esamilaboratorioparametri.merge(patients)
 esamilaboratorioparametri.data = pandas.to_datetime(esamilaboratorioparametri.data)
 esamilaboratorioparametri.codiceamd = esamilaboratorioparametri.codiceamd.astype(codice_amd.dtype)
 
-esamilaboratorioparametricalcolati = esamilaboratorioparametricalcolati.drop(esamilaboratorioparametricalcolati.columns[0], axis=1)
+esamilaboratorioparametricalcolati = remove_first_column(esamilaboratorioparametricalcolati)
 esamilaboratorioparametricalcolati = esamilaboratorioparametricalcolati.merge(patients)
 esamilaboratorioparametricalcolati.data = pandas.to_datetime(esamilaboratorioparametricalcolati.data)
 esamilaboratorioparametricalcolati.codiceamd = esamilaboratorioparametricalcolati.codiceamd.astype(codice_amd.dtype)
 esamilaboratorioparametricalcolati.codicestitch = esamilaboratorioparametricalcolati.codicestitch.astype(codice_stitch.dtype)
 
-esamistrumentali = esamistrumentali.drop(esamistrumentali.columns[0], axis=1)
+esamistrumentali = remove_first_column(esamistrumentali)
 esamistrumentali = esamistrumentali.merge(patients)
 esamistrumentali.data = pandas.to_datetime(esamistrumentali.data)
 esamistrumentali.codiceamd = esamistrumentali.codiceamd.astype(codice_amd.dtype)
 esamistrumentali.valore = esamistrumentali.valore.astype('category')
 
-prescrizionidiabetefarmaci = prescrizionidiabetefarmaci.drop(prescrizionidiabetefarmaci.columns[0], axis=1)
+prescrizionidiabetefarmaci = remove_first_column(prescrizionidiabetefarmaci)
 prescrizionidiabetefarmaci = prescrizionidiabetefarmaci.merge(patients)
 prescrizionidiabetefarmaci.data = pandas.to_datetime(prescrizionidiabetefarmaci.data)
 # NOTE: A10BD is a probably malformed.
 prescrizionidiabetefarmaci.codiceatc = prescrizionidiabetefarmaci.codiceatc.astype('category')
 
-prescrizionidiabetenonfarmaci = prescrizionidiabetenonfarmaci.drop(prescrizionidiabetenonfarmaci.columns[0], axis=1)
+prescrizionidiabetenonfarmaci = remove_first_column(prescrizionidiabetenonfarmaci)
 prescrizionidiabetenonfarmaci = prescrizionidiabetenonfarmaci.merge(patients)
 prescrizionidiabetenonfarmaci.data = pandas.to_datetime(prescrizionidiabetenonfarmaci.data)
 prescrizionidiabetenonfarmaci.codiceamd = prescrizionidiabetenonfarmaci.codiceamd.astype(codice_amd.dtype)
 # TODO: understand valore
 
-prescrizioninondiabete = prescrizioninondiabete.drop(prescrizioninondiabete.columns[0], axis=1)
+prescrizioninondiabete = remove_first_column(prescrizioninondiabete)
 prescrizioninondiabete = prescrizioninondiabete.merge(patients)
 prescrizioninondiabete.data = pandas.to_datetime(prescrizioninondiabete.data)
 prescrizioninondiabete.codiceamd = prescrizioninondiabete.codiceamd.astype(codice_amd.dtype)
 # TODO: understand valore, is it categorical?
 
-del patients
+del remove_first_column, patients
 
 # Tasks 1 ######################################################################
 

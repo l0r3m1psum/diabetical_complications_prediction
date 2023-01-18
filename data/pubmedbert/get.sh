0b9eb878714d7d75e4f2876086caa787a7294b4a
l0r3m1psum <bellanidiego@gmail.com>
2023-01-18 17:03:49 +0100

    Added a way to dowload PubMedBERT.

---
+++ data/pubmedbert/get.sh
@@ -0,0 +1,9 @@
+curl -zsL \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/.gitattributes' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/LICENSE.md' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/README.md' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/config.json' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/flax_model.msgpack' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/pytorch_model.bin' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/tokenizer_config.json' \
+	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/vocab.txt'

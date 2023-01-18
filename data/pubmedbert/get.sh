6e0c2f0be3ba55606c03752c5450d201ae5f16ee
l0r3m1psum <bellanidiego@gmail.com>
2023-01-18 17:52:42 +0100

    Corrected PubMedBERT download

--- data/pubmedbert/get.sh
+++ data/pubmedbert/get.sh
@@ -1,9 +1,9 @@
-curl -zsL \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/.gitattributes' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/LICENSE.md' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/README.md' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/config.json' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/flax_model.msgpack' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/pytorch_model.bin' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/tokenizer_config.json' \
-	-sO 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/vocab.txt'
+curl -Z \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/.gitattributes' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/LICENSE.md' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/README.md' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/config.json' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/flax_model.msgpack' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/pytorch_model.bin' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/tokenizer_config.json' \
+	-sOL 'https://huggingface.co/microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract/resolve/main/vocab.txt'
